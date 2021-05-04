9e604d718f7bf62177bad05b3d850327100afb9c
Muhamed Hobi <woohoomoo2u@gmail.com>
2021-05-04 18:25:48 -0400

    Correct permissions

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Install Wallpaper-aerial
 
-if [ ! $(id -u) = 0 ]; then
-	echo "Must be run as root."
-	exit 1
-fi
+# if [ ! $(id -u) = 0 ]; then
+# 	echo "Must be run as root."
+# 	exit 1
+# fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 git submodule update --init --recursive
 
-sudo apt-get install -y xorg-dev build-essential libx11-dev x11proto-xext-dev libxrender-dev libxext-dev mpv
+sudo apt-get install -y xorg-dev build-essential libx11-dev x11proto-xext-dev libxrender-dev libxext-dev mpv python3-dev python2-dev
 
 cd $DIR
 cd xwinwrap
@@ -20,12 +20,17 @@ cd ..
 mkdir -p /usr/lib/atvvid
 mkdir -p $HOME/.config/atv4wall/vid2
 mkdir -p $HOME/.mozilla/native-messaging-hosts
-sudo cp atv4wall /usr/bin/atv4wall && chmod a+x /usr/bin/atv4wall
-sudo cp vid /usr/bin/atvvid && chmod a+x /usr/bin/atvvid
-sudo cp vid2 $HOME/.config/atv4wall/vid2 && chmod a+x $HOME/.config/atv4wall/vid2
-sudo cp fxvid.json $HOME/.mozilla/native-messaging-hosts/fxvid.json
-cp wallvid.png /usr/lib/atvvid/
-chmod a+r /usr/lib/atvvid/*.png
+sudo cp atv4wall /usr/bin/atv4wall
+sudo chmod a+x /usr/bin/atv4wall
+python2 -m pip install pycairo
+python2 -m pip install PyGObject
+sudo cp vid /usr/bin/atvvid
+sudo chmod a+x /usr/bin/atvvid
+sudo cp vid2 /usr/lib/atvvid/vid2
+sudo chmod a+x /usr/lib/atvvid/vid2
+cp fxvid.json $HOME/.mozilla/native-messaging-hosts/fxvid.json
+sudo cp wallvid.png /usr/lib/atvvid/
+sudo chmod a+r /usr/lib/atvvid/*.png
 
 read -n1 -p "Autostart Aerial Wallpaper? (y/N)"
 echo "
